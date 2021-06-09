f405d8569ecc000a4abb8257f2a71f83cd162782
MeBs2121 <react2130@yahoo.co.jp>
2021-06-09 18:46:30 +0900

    installed Action Cable and logged when connected

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,20 @@
+import consumer from "./consumer";
+
+consumer.subscriptions.create("RoomChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+    console.log("connected!");
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+  },
+
+  speak: function () {
+    return this.perform("speak");
+  },
+});
