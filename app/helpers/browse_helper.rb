c92839290f05d028fa9a402d0647889d98873ae8
MeBs2121 <react2130@yahoo.co.jp>
2021-07-01 19:26:56 +0900

    edited tab

--- app/helpers/browse_helper.rb
+++ app/helpers/browse_helper.rb
@@ -3,4 +3,9 @@ module BrowseHelper
     @room = current_account.rooms & account.rooms
     @room.pluck(:id)[0]
   end
+
+  def last_message account
+    msg = account.messages.where(room_id: find_room(account)).last
+    msg.content
+  end
 end
